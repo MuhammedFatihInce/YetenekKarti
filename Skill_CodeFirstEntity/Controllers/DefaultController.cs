234f0678075e98d0bc0c46b9da90186b94e15253
Codcu-entel <88192139+Codcu-entel@users.noreply.github.com>
2023-02-22 13:53:41 +0300

    Asp.Net Yetenek Kartı Projesi
    Asp.NEt MVC ve Entity Framework Codefirst  ile Yetenek Kartı projesi

---
+++ Skill_CodeFirstEntity/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Skill_CodeFirstEntity.Models.siniflar;
+
+namespace Skill_CodeFirstEntity.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+			CONTEXT c = new CONTEXT();
+			var degerler = c.YETENEKLERS.ToList();
+            return View(degerler);
+        }
+    }
+}
